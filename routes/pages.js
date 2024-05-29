91d1ba49896971239fefe0788ad86c8b6f145093
AvArtAlek <159720522+AvArtAlek@users.noreply.github.com>
2024-05-29 21:21:48 +0300

    Commit to commit

---
+++ routes/pages.js
@@ -0,0 +1,10 @@
+const pagesRouter = require("express").Router();
+const {sendIndex,sendDashboard}=require('../controllers/auth');
+const {checkAuth,checkCookiesJWT}=require('../middllewares/auth');
+pagesRouter.get("/", sendIndex);
+
+// routes/pages.js
+// Импорты и другие маршруты
+
+pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
+module.exports=pagesRouter;

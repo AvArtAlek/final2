962246e070708fa17af1bd83de8d877433e528d5
AvArtAlek <159720522+AvArtAlek@users.noreply.github.com>
2024-05-30 18:07:03 +0300

    Финальное ревью

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pindie-backend-main
- 
+Ссылка на репозиторий с фронтендом: https://github.com/AvArtAlek/final2.2.git
+
+Ссылка на домен фронтенда: https://pindie-test-frontend.nomorepartiesco.ru/
+
+IP-адрес: 158.160.168.108
--- middllewares/cors.js
+++ middllewares/cors.js
@@ -1,4 +1,6 @@
-const allowedCors = ["https://localhost:3000", "https://localhost:3001",'https://pindie.portal.nomoredomainswork.ru','http://pindie.portal.nomoredomainswork.ru','https://portal.game.pindie.nomoredomainswork.ru','http://portal.game.pindie.nomoredomainswork.ru'];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://pindie-frontend.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
@@ -8,8 +10,7 @@ function cors(req, res, next) {
   }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
-module.exports=cors;
+
+module.exports = cors;
